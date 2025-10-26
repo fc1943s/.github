dc8c93529afdad66b4279fd9bd62c0d2ca06dccd
프리마 <fc1943s@pm.me>
2025-10-26 12:41:38 -0400

    tests

---
+++ scripts/init.ps1
@@ -0,0 +1,34 @@
+param(
+    $fast,
+    $ScriptDir = $PSScriptRoot
+)
+Set-Location $ScriptDir
+$ErrorActionPreference = "Stop"
+
+
+Set-Location (New-Item -ItemType Directory -Path "../.." -Force)
+git clone --recurse-submodules https://github.com/fc1943s/spiral.git # --branch gh-pages
+Set-Location spiral
+git pull
+Set-Location $ScriptDir
+
+pwsh ../../spiral/scripts/init.ps1
+
+
+. ../../polyglot/scripts/core.ps1
+
+Set-Location (New-Item -ItemType Directory -Path "../.." -Force)
+git clone --recurse-submodules https://github.com/fc1943s/dice.git # --branch gh-pages
+Set-Location dice
+git pull
+Set-Location $ScriptDir
+
+{ pwsh ../../dice/scripts/init.ps1 } | Invoke-Block
+
+Set-Location (New-Item -ItemType Directory -Path "../.." -Force)
+git clone --recurse-submodules https://github.com/fc1943s/alphabet.git # --branch gh-pages
+Set-Location alphabet
+git pull
+Set-Location $ScriptDir
+
+{ pwsh ../../alphabet/scripts/init.ps1 } | Invoke-Block
